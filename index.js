24450f23d040eb29d73cf6e5de7d3d8a15a5324e
rdtfyguhi8njhu <gs1elasante@hotmail.com>
2024-05-09 13:16:04 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-function maxEnvelopes(envelopes) {
-  envelopes.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
-  const dp = new Array(envelopes.length).fill(1);
-  for (let i = 0; i < envelopes.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (envelopes[i][1] > envelopes[j][1]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-      }
-    }
-  }
-  return Math.max(...dp);
-}
+const deepFlatten = (arr) =>
+  [].concat(...arr.map((v) => (Array.isArray(v) ? deepFlatten(v) : v)));
